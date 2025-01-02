63260476c0cb9a326e11be6e1e9c13476c1ed98b
SilverHawks <thekeke60@gmail.com>
2025-01-03 00:18:04 +0100

    Update de la boucle de base

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ from src.interface.mainmenu import MainMenu
 
 if __name__ == '__main__':
     root = tk.Tk()
+    
+    root.attributes('-fullscreen', True)
+    root.configure(bg='white')
+
     main_menu = MainMenu(root)
     root.mainloop()
+    
